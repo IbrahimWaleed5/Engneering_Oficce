7b5a8ad60a0aa942be0fa643b86f601828436601
LoayDev1 <loayfff6@gmail.com>
2026-07-19 15:40:49 +0300

    load vite assets in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,10 +38,7 @@
         rel="stylesheet"
     >
 
-    @vite([
-        'resources/css/app.css',
-        'resources/js/app.js'
-    ])
+   @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
