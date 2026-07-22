c524e160c164793c97287cba8d8cf3d3ddd24cd5
LoayDev1 <loayfff6@gmail.com>
2026-07-22 12:20:22 +0300

    Fix guest layout width and remove Laravel logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,48 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    dir="rtl"
+>
+<head>
+    <meta charset="utf-8">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <meta
+        name="csrf-token"
+        content="{{ csrf_token() }}"
+    >
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <title>
+        {{ config('app.name', 'مكتب الوليد الهندسي') }}
+    </title>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+    <link
+        rel="preconnect"
+        href="https://fonts.bunny.net"
+    >
+
+    <link
+        href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap"
+        rel="stylesheet"
+    >
+
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.js'
+    ])
+</head>
+
+<body class="font-sans antialiased text-white bg-slate-950">
+
+    <main
+        class="flex items-center justify-center w-full min-h-screen overflow-x-hidden"
+    >
+        {{ $slot }}
+    </main>
+
+</body>
 </html>
