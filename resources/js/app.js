fb8b34baa6850df4402d3984254233c5c55b0c25
LoayDev1 <loayfff6@gmail.com>
2026-07-24 02:18:32 +0300

    Fix profile tabs and Alpine behavior

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,6 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
 document.addEventListener('DOMContentLoaded', () => {
     const animatedElements = document.querySelectorAll(
