2d8ec3504947452fb68373070750085101a85d79
LoayDev1 <loayfff6@gmail.com>
2026-07-31 02:31:05 +0300

    Remove missing phone input import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,6 @@
 import './bootstrap';
-import './phone-input';
-
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
 document.addEventListener('DOMContentLoaded', () => {
     const animatedElements = document.querySelectorAll(
