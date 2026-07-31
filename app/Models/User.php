ddfdebe331409d76c7ea6ef602f0ce74f86edef0
LoayDev1 <loayfff6@gmail.com>
2026-07-31 15:34:01 +0300

    Fix Fortify email verification page

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
+
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory;
--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Str;
 use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
 use Laravel\Fortify\Fortify;
 
+
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
@@ -52,5 +53,8 @@ class FortifyServiceProvider extends ServiceProvider
                 ($credentialId ?: $request->session()->getId()).'|'.$request->ip()
             );
         });
+        Fortify::verifyEmailView(function () {
+    return view('auth.verify-email');
+});
     }
 }
